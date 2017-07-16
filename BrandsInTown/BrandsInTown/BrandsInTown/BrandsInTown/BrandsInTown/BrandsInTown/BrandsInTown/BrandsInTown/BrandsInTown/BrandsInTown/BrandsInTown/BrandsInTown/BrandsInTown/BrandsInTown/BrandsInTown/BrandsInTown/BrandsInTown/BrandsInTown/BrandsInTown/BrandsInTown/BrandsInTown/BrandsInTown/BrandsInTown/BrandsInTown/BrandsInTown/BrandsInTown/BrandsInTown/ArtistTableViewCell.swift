04956a6fcdee57c4812e32c73d9a24f0c4914bbd
mikhaillyapich <evildevil676@gmail.com>
2017-07-16 12:25:08 +0300

    xcode errors

---
+++ BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/BrandsInTown/ArtistTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtistTableViewCell.swift
+//  BrandsInTown
+//
+//  Created by Mikhail Lyapich on 7/15/17.
+//  Copyright © 2017 Mikhail Lyapich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+    
+}
---
+++ BrandsInTown/DataProvider.swift
@@ -0,0 +1,9 @@
+//
+//  DataProvider.swift
+//  BrandsInTown
+//
+//  Created by Mikhail Lyapich on 7/15/17.
+//  Copyright © 2017 Mikhail Lyapich. All rights reserved.
+//
+
+import Foundation
